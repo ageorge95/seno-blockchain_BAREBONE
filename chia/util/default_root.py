dda092e7d06935524c57a8597e5964754bf0bd2c
George Daniel Arteni <arteni.george.daniel@gmail.com>
2022-07-02 14:48:37 +0300

    added seno customization

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
 
-DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/mainnet"))).resolve()
+DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.seno2/mainnet"))).resolve()
 STANDALONE_ROOT_PATH = Path(
-    os.path.expanduser(os.getenv("CHIA_STANDALONE_WALLET_ROOT", "~/.chia/standalone_wallet"))
+    os.path.expanduser(os.getenv("CHIA_STANDALONE_WALLET_ROOT", "~/.seno2/standalone_wallet"))
 ).resolve()
 
-DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.chia_keys"))).resolve()
+DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.seno2_keys"))).resolve()
